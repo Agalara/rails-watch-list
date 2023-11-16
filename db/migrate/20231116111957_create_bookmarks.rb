e65053bc015bc696c15550901362f72d386ac157
alberto garcia <garcia.lara.alberto@gmail.com>
2023-11-16 15:53:07 +0100

    seeds added

--- db/migrate/20231116111957_create_bookmarks.rb
+++ db/migrate/20231116111957_create_bookmarks.rb
@@ -1,7 +1,7 @@
 class CreateBookmarks < ActiveRecord::Migration[7.1]
   def change
     create_table :bookmarks do |t|
-      t.string :comment
+      t.text :comment
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_16_112549) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_16_132743) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookmarks", force: :cascade do |t|
-    t.string "comment"
+    t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "movies_id"
-    t.bigint "lists_id"
-    t.index ["lists_id"], name: "index_bookmarks_on_lists_id"
-    t.index ["movies_id"], name: "index_bookmarks_on_movies_id"
+    t.bigint "movie_id"
+    t.bigint "list_id"
+    t.index ["list_id"], name: "index_bookmarks_on_list_id"
+    t.index ["movie_id"], name: "index_bookmarks_on_movie_id"
   end
 
   create_table "lists", force: :cascade do |t|
@@ -34,11 +34,11 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_16_112549) do
     t.string "title"
     t.string "overview"
     t.string "poster_url"
-    t.integer "rating"
+    t.float "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "bookmarks", "lists", column: "lists_id"
-  add_foreign_key "bookmarks", "movies", column: "movies_id"
+  add_foreign_key "bookmarks", "lists"
+  add_foreign_key "bookmarks", "movies"
 end
